8eb1025e577f7aaff32df81598ac6e503914118d
Ching-Hsin Lee <chinglee@amazon.com>
2021-02-05 10:52:32 +0800

    Add FreeMATICS sample OBD code
    Add FreeMATICS sample OBD code

--- vendors/espressif/boards/esp32/aws_demos/application_code/arduino_main.cpp
+++ vendors/espressif/boards/esp32/aws_demos/application_code/arduino_main.cpp
@@ -1,7 +1,78 @@
 #include "Arduino.h"
 
+#include "FreematicsPlus.h"
+
+FreematicsESP32 sys;
+COBD obd;
+bool connected = false;
+unsigned long count = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, HIGH);
+  delay(1000);
+  digitalWrite(PIN_LED, LOW);
+  Serial.begin(115200);
+
+  // initializations
+  while (!sys.begin());
+  obd.begin(sys.link);
+}
+
+void loop() {
+  digitalWrite(PIN_LED, HIGH);
+  // put your main code here, to run repeatedly:
+  if (!connected) {
+    digitalWrite(PIN_LED, HIGH);
+    Serial.print("Connecting to OBD...");
+    if (obd.init()) {
+      Serial.println("OK");
+      connected = true;
+    } else {
+      Serial.println();
+    }
+    digitalWrite(PIN_LED, LOW);
+    return;
+  }
+
+  int value;
+  Serial.print('[');
+  Serial.print(millis());
+  Serial.print("] #");
+  Serial.print(count++);
+  if (obd.readPID(PID_RPM, value)) {
+    Serial.print(" RPM:");
+    Serial.print(value);
+  }
+  if (obd.readPID(PID_SPEED, value)) {
+    Serial.print(" SPEED:");
+    Serial.print(value);
+  }
+
+  Serial.print(" BATTERY:");
+  Serial.print(obd.getVoltage());
+  Serial.print('V');
+
+  Serial.print(" CPU TEMP:");
+  Serial.print(readChipTemperature());
+  Serial.println();
+  if (obd.errors > 2) {
+    Serial.println("OBD disconnected");
+    connected = false;
+    obd.reset();
+  }
+  digitalWrite(PIN_LED, LOW);
+
+  delay(100);
+}
+
+
 extern "C" void arduino_main( void )
 {
     Serial.begin(115200);
     Serial.print("Arduino start\r\n");
+    
+    setup();
+    loop();
 }
